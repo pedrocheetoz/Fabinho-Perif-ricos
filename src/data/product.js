8611dfa1a492e4fd86540181887d8a3f24ec67e7
pedrocheetoz <schetzpedro297@gmail.com>
2026-06-09 10:45:48 -0300

    gole

--- src/data/product.js
+++ src/data/product.js
@@ -8,7 +8,7 @@ const produtos = [
   {id: 6, nome: 'Mouse Pad Gamer Exbom', desc: '', preco: 110, categoria: 'Mousepad', imagem: '/...'},
   {id: 7, nome: 'Mouse Gamer Usb Led Rgb 1200dpi Exbom - MS-62', desc: '', preco: 20, categoria: 'Mouse', imagem: '/...'},
   {id: 8, nome: 'PTKRWG75RD PCYes teclado mecanico Kirin 75% switch red', desc: '', preco: 216, categoria: 'Teclado', imagem: '/...'},
-  {id: 9, nome: 'Teclado Gamer Redragon Deimos K599-KNS – Mecânico 70% Compacto, Switch Marrom, RGB por Tecla, Hotswap, ABNT2, USB-C e Wireless 2.4GHz', desc: '', preco: 409,90, categoria: 'Teclado', imagem: '/...'},
+  {id: 9, nome: 'Teclado Gamer Redragon Deimos K599-KNS – Mecânico 70% Compacto, Switch Marrom, RGB por Tecla, Hotswap, ABNT2, USB-C e Wireless 2.4GHz', desc: '', preco: 409.90 ,  categoria: 'Teclado', imagem: '/...'},
   {id: 10, nome: '', desc: '', preco: , categoria: '', imagem: '/...'},
   {id: 11, nome: '', desc: '', preco: , categoria: '', imagem: '/...'},
   {id: 12, nome: '', desc: '', preco: , categoria: '', imagem: '/...'};
